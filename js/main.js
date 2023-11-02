7deebc5288b2c626173a3fe730e615793e6f89d3
Dontecare <Dontecare1@gmail.com>
2023-11-02 10:42:31 -0500

    finished about and home page basic layout

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// JS scripts placed here
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function mySecondFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "monday-soap") {
+        x.className += " responsive";
+    } else {
+        x.className = "monday-soap";
+    }
+}
+
+// adds active class to the body element
+function toggleActive(element) {
+    element.closest('.aqua-turtle-main').classList.toggle('active');
+}
